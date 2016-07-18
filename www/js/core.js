2daa44a990a1207dab5685e63d9c17aaca89fad8
kwetu <kwetudesign@gmail.com>
2016-07-18 14:53:27 +0300

    improved file
    improve

--- www/js/core.js
+++ www/js/core.js
@@ -151,7 +151,7 @@ app.filter('htmlToPlaintext', function() {
 
 app.controller('newsController', [ '$http', '$scope', '$rootScope', function($http, $scope, $rootScope){
 
-	$scope.yourAPI = 'http://kwetudesign.co.ke/blog/api/get_recent_posts/';
+	$scope.yourAPI = 'http://hugeclan.com/api/get_recent_posts/';
 	$scope.items = [];
 	$scope.totalPages = 0;
 	$scope.currentPage = 1;
